6b7c99e07e59d3b248222b73a34734634b02c570
cammy rapson <accounts@virtualhelperuk.com>
2025-01-04 20:39:48 +0000

    code flow init

--- auth/standard.go
+++ auth/standard.go
@@ -1,12 +1,136 @@
 package auth
 
-import "strings"
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"strings"
+)
 
 // Redirect User To:
 // https://login.xero.com/identity/connect/authorize?response_type={code}&client_id={YOURCLIENTID}&redirect_uri={YOURREDIRECTURI}&scope={openid profile email accounting.transactions}&state={123}
 // User is Returned to your redirect_uri with a code and a state
+// Exchange Code For Access Token (optional: refresh token, id token)
 
 // This function is used to generate the URL that the user will be redirected to in order to authenticate with Xero.
 func NewAuthRedirectUrl(response_type string, client_id string, redirect_uri string, scope []string, state string) string {
 	return "https://login.xero.com/identity/connect/authorize?response_type=" + response_type + "&client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&scope=" + strings.Join(scope, " ") + "&state=" + state
+}
+
+var (
+	ErrMisMatchedState         = errors.New("state mismatch")
+	ErrInvalidInput            = errors.New("one or more inputs are invalid")
+	ErrOfflineAccessNotEnabled = errors.New("in order to refresh tokens, offline access scope must be provided at setup")
+)
+
+func ExchangeCode(code, clientID, clientSecret, redirectURI string) (identityToken, accessToken, refreshToken string, err error) {
+	if code == "" || clientID == "" || clientSecret == "" {
+		err = ErrInvalidInput
+		return
+	}
+	url := "https://identity.xero.com/connect/token"
+	authHeader := "Basic " + base64.StdEncoding.EncodeToString([]byte(clientID+":"+clientSecret))
+	type tokenRequest struct {
+		Grant_type   string `json:"grant_type"`
+		Code         string `json:"code"`
+		Redirect_uri string `json:"redirect_uri"`
+	}
+	var reqBody = tokenRequest{
+		Grant_type:   "authorization_code",
+		Code:         code,
+		Redirect_uri: redirectURI,
+	}
+	b, err := json.Marshal(reqBody)
+	if err != nil {
+		return
+	}
+	buf := bytes.NewBuffer(b)
+	request, err := http.NewRequest("POST", url, buf)
+	if err != nil {
+		return
+	}
+	request.Header.Add("Authorization", authHeader)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response, err := authClient.Do(request)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+	b, err = io.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+	if response.StatusCode != 200 {
+		err = errors.New(string(b))
+		return
+	}
+
+	type responseBody struct {
+		AccessToken   string `json:"access_token"`
+		RefreshToken  string `json:"refresh_token"`
+		IdentityToken string `json:"id_token"`
+	}
+	var resp = &responseBody{}
+	err = json.Unmarshal(b, resp)
+	if err != nil {
+		return
+	}
+	accessToken = resp.AccessToken
+	refreshToken = resp.RefreshToken
+	identityToken = resp.IdentityToken
+	return
+}
+
+func RefreshToken(clientId, clientSecret, RefreshToken string) (identityToken, accessToken, refreshToken string, err error) {
+	url := "https://identity.xero.com/connect/token"
+	authHeader := "Basic " + base64.StdEncoding.EncodeToString([]byte(clientId+":"+clientSecret))
+	type tokenRequest struct {
+		GrantType    string `json:"grant_type"`
+		RefreshToken string `json:"refresh_token"`
+	}
+	var reqBody = tokenRequest{
+		GrantType:    "refresh_token",
+		RefreshToken: RefreshToken,
+	}
+	b, err := json.Marshal(reqBody)
+	if err != nil {
+		return
+	}
+	buf := bytes.NewBuffer(b)
+	request, err := http.NewRequest("POST", url, buf)
+	if err != nil {
+		return
+	}
+	request.Header.Add("Authorization", authHeader)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response, err := authClient.Do(request)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+	b, err = io.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+	if response.StatusCode != 200 {
+		err = errors.New(string(b))
+		return
+	}
+	type responseBody struct {
+		AccessToken   string `json:"access_token"`
+		RefreshToken  string `json:"refresh_token"`
+		IdentityToken string `json:"id_token"`
+	}
+	var resp = &responseBody{}
+	err = json.Unmarshal(b, resp)
+	if err != nil {
+		return
+	}
+	accessToken = resp.AccessToken
+	refreshToken = resp.RefreshToken
+	identityToken = resp.IdentityToken
+	return
 }
--- client/client.go
+++ client/client.go
@@ -1,4 +1,77 @@
 package client
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/VirtualHelperUK/xerogo/auth"
+)
+
 type client struct {
+	clientID      string
+	clientSecret  string
+	redirectURI   string
+	scope         []string
+	accessToken   string // Expiry: 30 minutes
+	refreshToken  string // Expiry: 60 days
+	identityToken string // Expiry: 5 minutes
+	lastRefresh   time.Time
 }
+
+// NewClient creates a new client object
+// redirectURI is the URL that the user will be redirected to after authenticating with Xero -- This must be an https address, however for testing you can use http://localhost/ Please note that http://127.0.0.1 cannot be used
+// scope is a list of the permissions that your app will need to access -- https://developer.xero.com/documentation/guides/oauth2/scopes/
+func NewClient(clientID string, clientSecret string, redirectURI string, scope []string) *client {
+	return &client{
+		clientID:     clientID,
+		clientSecret: clientSecret,
+		redirectURI:  redirectURI,
+		scope:        scope,
+	}
+}
+
+// GetClientID returns the client ID
+func (c *client) GetClientID() string {
+	return c.clientID
+}
+
+func (c *client) GetStandardAuthRedirectURL() (url string, state string) {
+	state = fmt.Sprintf("%d", rand.Int31n(1000))
+	url = auth.NewAuthRedirectUrl("code", c.clientID, c.redirectURI, c.scope, state)
+	return
+}
+
+func (c *client) VerifyStandardAuthRedirectCode(code, state, expectedState string) (err error) {
+	if state != expectedState {
+		return auth.ErrMisMatchedState
+	}
+	identityToken, accessToken, refreshToken, err := auth.ExchangeCode(code, c.clientID, c.clientSecret, c.redirectURI)
+	if err != nil {
+		return
+	}
+	c.identityToken = identityToken
+	c.accessToken = accessToken
+	c.refreshToken = refreshToken
+	c.lastRefresh = time.Now()
+	return
+}
+
+func (c *client) RefreshTokens() (err error) {
+	if c.refreshToken == "" {
+		return auth.ErrOfflineAccessNotEnabled
+	}
+	identityToken, accessToken, refreshToken, err := auth.RefreshToken(c.refreshToken, c.clientID, c.clientSecret)
+	if err != nil {
+		return
+	}
+	c.identityToken = identityToken
+	c.accessToken = accessToken
+	c.refreshToken = refreshToken
+	c.lastRefresh = time.Now()
+	return
+}
+
+// func (c client) requiresRefresh() bool {
+// 	return time.Since(c.lastRefresh) > 30*time.Minute
+// }
